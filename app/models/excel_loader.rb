96cc99caa6221e7896820cadbbcf883c69f0292e
lukepfeiffer <lukerpfeiffer@gmail.com>
2019-01-19 14:23:35 -0500

    More generic data loader

--- app/models/excel_loader.rb
+++ app/models/excel_loader.rb
@@ -3,12 +3,19 @@ require 'csv'
 require 'pry'
 
 class ExcelLoader
-  def self.load_data(file)
+  def self.load_data(file, option)
     counties = Hash.new
     #right now just waste data
+    val_index = -1
+    if option == "food_waste"
+      val_index = 11
+    elsif option == "recycle_waste"
+      val_index = 12
+    end
+
     CSV.foreach(file) do |row|
       #binding.pry
-      counties[row[0]] = row[11].gsub(",","").to_f / row[1].gsub(",","").to_f
+      counties[row[0]] = row[val_index].gsub(",","").to_f / row[1].gsub(",","").to_f
     end
     counties
   end
