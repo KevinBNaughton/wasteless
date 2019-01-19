9365b9e3bbb8617dcf0c53f033c1252ea3b7e379
Kevin Naughton <kevinbnaughton@gmail.com>
2019-01-19 14:16:55 -0500

    excel_loader.rb takes in waste data

--- app/models/excel_loader.rb
+++ app/models/excel_loader.rb
@@ -1,11 +1,16 @@
 require 'json'
+require 'csv'
+require 'pry'
 
 class ExcelLoader
   def self.load_data(file)
     counties = Hash.new
+    #right now just waste data
     CSV.foreach(file) do |row|
-      
+      #binding.pry
+      counties[row[0]] = row[11].gsub(",","").to_f / row[1].gsub(",","").to_f
     end
+    counties
   end
 
 end
